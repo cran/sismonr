ca439abb3a90d6e7ec9e1d27c1a2b6ba2b9e108e
Olivia Angelin-Bonnet <olivia.angelinbonnet@gmail.com>
2020-02-11 05:50:02 +0000

    version 2.1.0

--- inst/julia/testModules.jl
+++ inst/julia/testModules.jl
@@ -30,10 +30,14 @@ end
 importExprBS = Meta.parse("import BioSimulator");
 try
 	eval(importExprBS)
+	if Pkg.installed()["BioSimulator"] < v"0.9.3"
+		Pkg.rm("BioSimulator")
+		Pkg.add(PackageSpec(url = "https://github.com/alanderos91/BioSimulator.jl.git", rev = "9f47b29de716a24ed7bb583920774c790d94c256"))
+	end
 	print("Module BioSimulator installed.\n")
 catch err
 	print("Installing Julia module Biosimulator. This can take a few minutes.\n")
-	Pkg.add(PackageSpec(url = "https://github.com/alanderos91/BioSimulator.jl.git"))
+	Pkg.add(PackageSpec(url = "https://github.com/alanderos91/BioSimulator.jl.git", rev = "9f47b29de716a24ed7bb583920774c790d94c256"))
  end
 
 importExprSB = Meta.parse("import StatsBase");
